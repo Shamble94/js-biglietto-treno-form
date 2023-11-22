3794c48c51143b8214c9cbec0f636f215efcbc93
Shamble94 <fratus.nicholas@gmail.com>
2023-11-22 17:20:23 +0100

    [FIXED] tofixed in else

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 
-let km = document.getElementById(`km`).value
-let eta = document.getElementById(`eta`).value
-let prezzo;
-let messaggio;
-let cliente = document.getElementById(`nome`).value + " " + document.getElementById(`cognome`).value 
 const button = document.getElementById(`btn-action`);
-console.log(km);
-console.log(eta)
-console.log(cliente) 
 
 button.addEventListener(`click`, function(){
 
+    let km = document.getElementById(`km`).value
+    let eta = document.getElementById(`eta`).value
+    let prezzo;
+    let cliente = document.getElementById(`nome`).value + " " + document.getElementById(`cognome`).value 
+    console.log(km);
+    console.log(eta)
+    console.log(cliente) 
+
+
 if(eta <= 18){
-   prezzo = (km * 0.21 * 0.8).toFixed(2);
-   console.log(prezzo);
-   messaggio = `Complimenti! ${cliente} lei rientra nella fascia under 18 e quindi ha diritto ad uno scontro del 20%, il suo biglietto costa: ${prezzo}`
+    prezzo = (km * 0.21 * 0.8).toFixed(2);
+    console.log(prezzo);
+    messaggio = `Complimenti! ${cliente} lei rientra nella fascia under 18 e quindi ha diritto ad uno scontro del 20%, il suo biglietto costa: ${prezzo}`
     console.log(messaggio)
     document.getElementById(`messaggio`).innerHTML = messaggio
 
@@ -24,7 +25,7 @@ if(eta <= 18){
     messaggio = `Complimenti! ${cliente} lei rientra nella fascia over 65 e quindi ha diritto ad uno scontro del 40%, il suo biglietto costa: ${prezzo}`
     document.getElementById(`messaggio`).innerHTML = messaggio
 }else{
-    prezzo = km * 0.21
+    prezzo = (km * 0.21).toFixed(2)
     console.log(prezzo)
     messaggio = `Salve ${cliente}  il suo biglietto costa: ${prezzo}`
     document.getElementById(`messaggio`).innerHTML = messaggio
